2ab6c3d3bba7d0b90fe76e04ed734d70e4527d2b
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-09 16:22:01 -0300

    se agregaron los componentes a las rutas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
+import { LoginAndRegister } from '../components/auth/LoginAndRegister'
+import { CalendarScreen } from '../components/calendar/CalendarScreen'
 
 export const AppRouter = () => {
     return (
         <Routes>
-            <Route />
-            <Route />
+            <Route path='/login' element={<LoginAndRegister />} />
+            <Route path='/' element={<CalendarScreen />}/>
         </Routes>
     )
 }
