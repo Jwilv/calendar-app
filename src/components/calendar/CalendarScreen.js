292b0c37466a09d47ec7317355a334e48c56341f
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-09 17:54:12 -0300

    se creo Navbar y se agrego el link para los logos

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Navbar } from '../user interface/Navbar'
 
 export const CalendarScreen = () => {
     return (
-        <div>CalendarScreen</div>
+        <Navbar />
     )
 }
