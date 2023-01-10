afabb906e93601e24ccbac80224fbe5a98cd2cb9
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-10 10:01:41 -0300

    se creo em modal

---
+++ src/components/calendar/CalendarModal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+Modal.setAppElement('#root');
+
+export const CalendarModal = () => {
+    return (
+        <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+        >
+        </Modal>
+    )
+}
