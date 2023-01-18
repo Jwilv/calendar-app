f11aeb4cd81c1586520b84b76503eaabb50ff0cc
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-18 16:31:57 -0300

    se agrego un nuevo estado

--- src/redux/calendar.slice.js
+++ src/redux/calendar.slice.js
@@ -4,13 +4,13 @@ import moment from "moment";
 const initialState = {
     events: [
         {
-            title:'funciona?',
-            start : moment().toDate(),
-            end : moment().add(2,'hours').toDate(),
+            title: 'funciona?',
+            start: moment().toDate(),
+            end: moment().add(2, 'hours').toDate(),
             bgcolor: '#fafafa',
-            user:{
-                id:'1234',
-                name:'juanceto'
+            user: {
+                id: '1234',
+                name: 'juanceto'
             }
         },
     ],
@@ -20,20 +20,23 @@ const initialState = {
 const calendarSlice = createSlice({
     name: 'calendar',
     initialState,
-    reducers:{
-        eventAddNew:(state, action)=>{
-
-        },
-        eventSetActive: (state, action)=>{
-            return{
+    reducers: {
+        eventAddNew: (state, action) => {
+            return {
                 ...state,
-                active:{...action.payload}
+                events:[...state.events,action.payload]
+            }
+        },
+        eventSetActive: (state, action) => {
+            return {
+                ...state,
+                active: { ...action.payload }
             }
         }
 
     }
 })
 
-export const {eventAddNew,eventSetActive} = calendarSlice.actions
+export const { eventAddNew, eventSetActive } = calendarSlice.actions
 
 export default calendarSlice.reducer;
