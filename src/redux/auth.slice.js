b5f7054ca016f46b7b4d5464c88b59b86bbb05ff
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-30 17:48:46 -0300

    se instalo thunkk y se creo authSlice

---
+++ src/redux/auth.slice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    checking: true,
+    // uid:null,
+    // name:null,
+}
+
+const authSlice = createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        
+    }
+})
