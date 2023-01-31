54a1cde69d450819c27df3e7aedea6850370042b
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-31 15:03:46 -0300

    se creo startRegister

--- src/redux/auth.slice.js
+++ src/redux/auth.slice.js
@@ -44,4 +44,23 @@ export const startLogin =  (email,password)=>{
     }
 }
 
+export const startRegister =  (email,password,name)=>{
+    return async(dispatch)=>{
+        const res = await fechWithoToken('auth/new',{email,password,name}, 'POST');
+        const body = await res.json();
+        
+        if(body.ok){
+            localStorage.setItem('token',body.token)
+            localStorage.setItem('token-init-date', new Date().getTime());
+
+            dispatch(login({
+                uid:body.uid,
+                name:body.name,
+            }))
+        }else{
+            Swal.fire('Error',body.msg,'error')
+        }
+    }
+}
+
 export default  authSlice.reducer;
