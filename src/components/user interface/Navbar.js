0447cc83461429aaa307d3993f63a0aab2512cc6
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-31 20:08:57 -0300

    logout terminado

--- src/components/user interface/Navbar.js
+++ src/components/user interface/Navbar.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { startLogout } from '../../redux/auth.slice';
 
 export const Navbar = () => {
+
+    const dispatch = useDispatch();
+
     const {name} = useSelector(state => state.auth)
+
+    const handleLogout = ()=>{
+        dispatch(startLogout());
+    }
+
     return (
         <div className='navbar navbar-dark bg-dark mb-4'>
             <span className='navbar-brand ms-3'>
                 {name}
             </span>
-            <button className='btn btn-outline-danger me-3'>
+            <button 
+            className='btn btn-outline-danger me-3'
+            onClick={handleLogout}
+            >
                 <i className='fas fa-sign-out-alt'></i>
                 <span> Salir</span>
             </button>
