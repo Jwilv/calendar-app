af868b10b44ce559f25604f2331ba9938ef30c67
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-31 20:00:03 -0300

    se muestra el name del user en navbar

--- src/components/user interface/Navbar.js
+++ src/components/user interface/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const Navbar = () => {
+    const {name} = useSelector(state => state.auth)
     return (
         <div className='navbar navbar-dark bg-dark mb-4'>
             <span className='navbar-brand ms-3'>
-                juancito
+                {name}
             </span>
             <button className='btn btn-outline-danger me-3'>
                 <i className='fas fa-sign-out-alt'></i>
