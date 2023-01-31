a9d76dd9e87efaa9b9a16f699794d6e8fb340abb
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-31 17:26:52 -0300

    se agrega SpinnerScreen

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router'
 import { LoginScreen } from '../components/auth/LoginScreen'
 import { CalendarScreen } from '../components/calendar/CalendarScreen'
@@ -7,9 +7,16 @@ import { startChecking } from '../redux/auth.slice'
 
 export const AppRouter = () => {
     const dispatch = useDispatch();
+
+    const { checking } = useSelector(state => state.auth);
+
     useEffect(() => {
         dispatch(startChecking())
     }, [dispatch])
+
+    if(checking){
+        return ( <SpinnerScreen />)
+    }
     
     return (
         <Routes>
