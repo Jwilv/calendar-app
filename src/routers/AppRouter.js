698107addd8dad8bcd5189fc6270201aab7efc91
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-01-31 17:04:13 -0300

    effect de startChecking

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Route, Routes } from 'react-router'
 import { LoginScreen } from '../components/auth/LoginScreen'
 import { CalendarScreen } from '../components/calendar/CalendarScreen'
+import { startChecking } from '../redux/auth.slice'
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(startChecking())
+    }, [dispatch])
+    
     return (
         <Routes>
             <Route path='/login' element={<LoginScreen />} />
