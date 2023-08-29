029c442c95bb4d0a0613e240a1fc359fa1f966c5
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-08-29 14:19:13 -0300

    delete service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CalendarApp } from './CalendarApp';
 import './styles.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,9 +9,3 @@ root.render(
     <CalendarApp />
 
 );
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
