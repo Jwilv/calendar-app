948f538714bd01cbd9993856cf713a3ef35406f8
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-08-30 22:16:20 -0300

    format

--- src/sw-template.js
+++ src/sw-template.js
@@ -3,38 +3,38 @@ workbox.loadModule('workbox-background-sync')
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-const {registerRoute} = workbox.routing
-const {CacheFirst, NetworkFirst, NetworkOnly} = workbox.strategies
-const {BackgroundSyncPlugin} = workbox.backgroundSync;
+const { registerRoute } = workbox.routing
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
 registerRoute(
-    new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css'),
-    new CacheFirst(),
+  new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css'),
+  new CacheFirst(),
 );
 
 registerRoute(
-    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'),
-    new CacheFirst(),
+  new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'),
+  new CacheFirst(),
 );
 
 registerRoute(
-    new RegExp('http://localhost:4000/api/auth/renew'),
-    new NetworkFirst(),
+  new RegExp('http://localhost:4000/api/auth/renew'),
+  new NetworkFirst(),
 );
 
 registerRoute(
-    new RegExp('http://localhost:4000/api/events'),
-    new NetworkFirst(),
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkFirst(),
 );
 
 const bgSyncPlugin = new BackgroundSyncPlugin('posts-offline', {
-    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
-  });
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
 
-  registerRoute(
-    new RegExp('http://localhost:4000/api/events'),
-    new NetworkOnly({
-      plugins: [bgSyncPlugin],
-    }),
-    'POST'
-  );
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'POST'
+);
