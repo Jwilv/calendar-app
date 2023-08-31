89b0ab0c05c3a27243867eed49a9798cfd0dad77
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-08-31 16:24:57 -0300

    online and offline navbar

--- src/components/user interface/Navbar.js
+++ src/components/user interface/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { startLogout } from '../../redux/auth.slice';
 
+import { Online, Offline} from 'react-detect-offline'
+
 export const Navbar = () => {
 
     const dispatch = useDispatch();
@@ -17,6 +19,14 @@ export const Navbar = () => {
             <span className='navbar-brand ms-3'>
                 {name}
             </span>
+
+            <Online>
+            <span className='text-success'>Online</span>
+            </Online>
+            <Offline>
+            <span className='text-danger'>Offline</span>
+            </Offline>
+
             <button 
             className='btn btn-outline-danger me-3'
             onClick={handleLogout}
